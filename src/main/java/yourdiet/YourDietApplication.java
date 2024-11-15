11586aab4ad89a20c69264c860993b429c6a7228
tombdf <thomas@hexasec.io>
2024-11-15 14:17:13 +0100

    feat: ajout de l'application Spring Boot principale
    Ajout de l'application Spring Boot principale avec le point d'entrée.

---
+++ src/main/java/yourdiet/YourDietApplication.java
@@ -0,0 +1,13 @@
+package yourdiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourDietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourDietApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yourdiet/YourDietApplicationTests.java
@@ -0,0 +1,13 @@
+package yourdiet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourDietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
