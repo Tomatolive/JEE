ee129cf914d256523464150038a56a5e8188e5d3
tombdf <thomas@hexasec.io>
2024-11-22 11:43:45 +0100

    feat(auth): ajouter la gestion des attributs d'inscription
    - Ajout de méthodes pour gérer les attributs d'inscription des utilisateurs
    - Mise à jour de la gestion des sessions pour le processus d'inscription
    - Amélioration de la gestion des erreurs et des retours utilisateurs lors de l'inscription
    - Mise à jour correcte des attributs après modification dans le profil

--- src/main/java/yourdiet/security/UserDetailsImpl.java
+++ src/main/java/yourdiet/security/UserDetailsImpl.java
@@ -1,22 +1,28 @@
 package yourdiet.security;
 
 import lombok.Getter;
-import yourdiet.model.User;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import yourdiet.model.User;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 public class UserDetailsImpl implements UserDetails {
     @Getter
     private final User user;
-    @Getter
     private final List<SimpleGrantedAuthority> authorities;
 
     public UserDetailsImpl(User user) {
         this.user = user;
         this.authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
     }
 
     @Override
@@ -27,5 +33,36 @@ public class UserDetailsImpl implements UserDetails {
     @Override
     public String getUsername() {
         return user.getUsername();
+    }
+
+    public Long getUserId() {
+        return user.getId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void updateUserData(User updatedUser) {
+        user.setAge(updatedUser.getAge());
+        user.setGender(updatedUser.getGender());
+        user.setWeight(updatedUser.getWeight());
+        user.setHeight(updatedUser.getHeight());
     }
 }
