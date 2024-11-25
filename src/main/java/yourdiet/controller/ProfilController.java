5544058ea71aea6eadf5c8bffef0961508be5fb8
cheaurizar <cheaurizard@proton.me>
2024-11-25 15:57:04 +0100

    Modification de la page de profil pour prendre en compte le niveau d'activité physique, modification de la page edit profil pour prendre en compte la selection du niveau d'activité physique et le genre et modification du dashboard

--- src/main/java/yourdiet/controller/ProfilController.java
+++ src/main/java/yourdiet/controller/ProfilController.java
@@ -21,7 +21,32 @@ public class ProfilController {
      */
     @GetMapping
     public String showProfilPage(@AuthenticationPrincipal UserDetailsImpl userDetails, Model model) {
+        String activityLevelLettre;
+        User user = userDetails.getUser();
+        Double activityLevel= user.getActivityLevel();
+        float activityLevelFloat = activityLevel.floatValue();
+        int activityLevelEntier = (int) (1000* activityLevelFloat);
+        switch (activityLevelEntier){
+            case 1200:
+                activityLevelLettre="Sédentaire - Pratiquemment aucun exercice quotidien";
+                break;
+            case 1375:
+                activityLevelLettre="Peu actif - Peu d'activité physique (1 à 3 par semaine, intensité modérée)";
+                break;
+            case 1550:
+                activityLevelLettre="Actif - Activité physique régulière (3 à 5 par semaine, intensité modérée)";
+                break;
+            case 1725:
+                activityLevelLettre="Très actif - Activité quotidienne (intensité soutenue)";
+                break;
+            case 1900:
+                activityLevelLettre="Extrêmement actif - Travail extrêmement physique ou grand sportif";
+                break;
+            default:
+                activityLevelLettre="Non déterminé";
+        }
         model.addAttribute("user", userDetails.getUser());
+        model.addAttribute("activityLevel",activityLevelLettre);
         return "profil";
     }
 
@@ -35,21 +60,22 @@ public class ProfilController {
 
     // Traitement de la soumission du formulaire de modification
     @PostMapping("/update")
-    public String updateProfil(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestParam Integer newAge, @RequestParam String newGender, @RequestParam Double newWeight, @RequestParam Double newHeight, Model model) {
+    public String updateProfil(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestParam Integer newAge, @RequestParam String newGender, @RequestParam Double newWeight, @RequestParam Double newHeight,@RequestParam Double newActivityLevel, Model model) {
 
         User user = userDetails.getUser();
 
         // Mettre à jour l'utilisateur dans la base de données
-        User updatedUser = userService.updateAge(user, newAge);
-        userService.updateGender(updatedUser, newGender);
-        userService.updateWeight(updatedUser, newWeight);
-        userService.updateHeight(updatedUser, newHeight);
+        userService.updateAge(user, newAge);
+        userService.updateGender(user, newGender);
+        userService.updateWeight(user, newWeight);
+        userService.updateHeight(user, newHeight);
+        userService.updateActivityLevel(user,newActivityLevel);
 
         // Mettre à jour les données dans UserDetails
-        userDetails.updateUserData(updatedUser);
+        userDetails.updateUserData(user);
 
         // Ajouter l'utilisateur mis à jour au modèle
-        model.addAttribute("user", updatedUser);
+        model.addAttribute("user", user);
 
         return "redirect:/profil";
     }
